7b3685b4ab4b57b7b8792a516a821c1068b8311e
Panos Mavrogiorgos <pmav99@gmail.com>
2023-11-15 09:35:08 +0200

    Expose check_results

--- multifutures/__init__.py
+++ multifutures/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import importlib
 
+from multifutures.multi import check_results
 from multifutures.multi import FutureResult
 from multifutures.multi import multiprocess
 from multifutures.multi import multithread
@@ -11,6 +12,7 @@ from multifutures.rate_limit import wait
 __version__ = importlib.metadata.version("multifutures")
 
 __all__: list[str] = [
+    "check_results",
     "FutureResult",
     "multiprocess",
     "multithread",
