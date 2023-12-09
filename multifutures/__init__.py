d2ea1d79965ef2e6af3d60d0d00e6232c361bae5
Panos Mavrogiorgos <pmav99@gmail.com>
2023-12-09 21:42:22 +0200

    expose MAX_AVAILABLE_PROCESSES

--- multifutures/__init__.py
+++ multifutures/__init__.py
@@ -4,6 +4,7 @@ import importlib
 
 from multifutures.multi import check_results
 from multifutures.multi import FutureResult
+from multifutures.multi import MAX_AVAILABLE_PROCESSES
 from multifutures.multi import multiprocess
 from multifutures.multi import multithread
 from multifutures.rate_limit import RateLimit
@@ -14,6 +15,7 @@ __version__ = importlib.metadata.version("multifutures")
 __all__: list[str] = [
     "check_results",
     "FutureResult",
+    "MAX_AVAILABLE_PROCESSES",
     "multiprocess",
     "multithread",
     "RateLimit",
